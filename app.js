26697089a67ecdddd94da4620eb6f71f760cf363
Jazz Lyles <ripley36706@gmail.com>
2016-08-21 12:52:32 -0700

    added OPD_OfficerContactInfo pdf2json parsing module to app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const OPDCalls = require('./PD_Data/OPDCalls');
+const OPD_Calls = require("./PD_Data/OPDCalls");
+const OPD_Officers = require("./PD_Data/OPD_OfficerContactInfo");
 
-OPDCalls();
+OPD_Calls();
+
+OPD_Officers();

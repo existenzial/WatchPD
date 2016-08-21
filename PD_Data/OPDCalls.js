c40eefe905f41533e7a8c85feb1956468df38a22
Jazz Lyles <ripley36706@gmail.com>
2016-08-21 12:07:29 -0700

    separated OPDCall data into exported data module for app

---
+++ PD_Data/OPDCalls.js
@@ -0,0 +1,24 @@
+const Converter = require("csvtojson").Converter;
+const converter = new Converter({});
+const ftp = require("ftp-get");
+
+module.exports = function(){
+  ftp.get({
+    url: process.env.OPD_URL,
+    bufferType: 'buffer'
+  }, function (error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+      result = result.toString();
+      converter.fromString(result, function(err, res){
+        //console.log('The CSV contents: ' + JSON.stringify(res));
+      });
+    }
+  });
+
+  //record_parsed emits each csv row
+  converter.on("record_parsed", function (policeDataObj) {
+    console.log( policeDataObj ); //parsed csv data
+  });
+};
